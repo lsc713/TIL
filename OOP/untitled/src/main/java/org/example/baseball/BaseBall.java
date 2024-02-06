2765f9d77e10ea0d74f53f811a986ca2644048c4
lsc713 <lsc7134.naver.com>
2024-02-06 14:32:32 +0900

    [ADD] baseball

--- OOP/untitled/src/main/java/org/example/baseball/BaseBall.java
+++ OOP/untitled/src/main/java/org/example/baseball/BaseBall.java
@@ -4,82 +4,62 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
-import static java.lang.Math.random;
+import static java.lang.Math.*;
+
 
 public class BaseBall {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-//        int  a = (int)(random() *10);
-//        int  b = (int)(random() *10);
-//        while (a == b){
-//            b = (int)(random() *10);
-//        }
-//        int c = (int) (random() * 10);
-//        while (c == a && c == b){
-//            c = (int) (random() * 10);
-//        }
-//        ArrayList<Integer> arr = new ArrayList<>();
-//
-//        arr.add(a);
-//        arr.add(b);
-//        arr.add(c);
 
-        int[] arr = new int[3];
-        arr[0] = (int) (random() * 10);
-        arr[1] = (int) (random() * 10);
-        while (arr[0] == arr[1]){
-            arr[1] = (int) (random() * 10);
+        int[] com = new int[3];
+        com[0] = (int) (random() * 10);
+        com[1] = (int) (random() * 10);
+        while (com[0] == com[1]){
+            com[1] = (int) (random() * 10);
         }
-        arr[2] = (int) (random() * 10);
-        while (arr[2] == arr[0] || arr[2] == arr[1]) {
-            arr[2] = (int) (random() * 10);
+        com[2] = (int) (random() * 10);
+        while (com[2] == com[0] || com[2] == com[1]) {
+            com[2] = (int) (random() * 10);
         }
 
-        System.out.println("arr = " + Arrays.toString(arr));
+        System.out.println("com = " + Arrays.toString(com));
 
-        int[] answer = new int[3];
+        int[] user = new int[3];
         System.out.println("컴퓨터가 숫자를 생성했습니다. 답을 맞춰보세요");
         int strike = 0;
         int ball = 0;
         int count = 0;
+
         while (true) {
             count++;
-
-            for (int i = 0; i < answer.length; i++) {
-                answer[i] = sc.nextInt();
-                if (arr[i] == answer[i]) {
-                    strike += 1;
-                }
-                if (answer[0] == answer[1] || answer[0] == answer[2] || answer[1] == answer[2]) {
-                    System.out.println("중복발생");
+            int number = sc.nextInt();
+            if(number < 100 || number > 1000){
+                continue;
+            }
+            user[0] = number / 100;
+            user[1] = (number % 100) / 10;
+            user[2] = number % 10;
+            if (user[0] == user[1] || user[0] == user[2] || user[1] == user[2]) {
+                System.out.println("중복발생");
+                continue;
+            }
+            for (int i = 0; i < user.length; i++) {
+                if(number < 100 || number > 1000){
+                    System.out.println("세자리숫자를 적어주세요");
                     continue;
                 }
-                for (int j = 0; j < answer.length; j++) {
-                    if (arr[j]==answer[i]){
-                        ball += 1;
+                for (int j = 0; j < user.length; j++) {
+                    if (com[j]==user[i]){
+                      if(i == j){
+                          strike++;
+                      } else{
+                          ball++;
+                      }
                     }
                 }
-//
-//            if (ball == 0) {
-//                System.out.println(strike+"S");
-//                strike=0;
-//                ball=0;
-//
-//            } else if (strike == 0) {
-//                System.out.println(ball + "B");
-//                strike=0;
-//                ball=0;
-//            } else {
-//                System.out.println(ball + "B" + strike + "S");
-//                strike=0;
-//                ball=0;
-//            }
-
             }
-            System.out.println(ball+"B"+strike+"S");
 
-
-
+            System.out.println(count+"번째 시도 : "+ball+"B"+strike+"S");
 
             if (strike == 3) {
                 System.out.println(count + "번만에 맞히셨습니다.");
@@ -93,3 +73,6 @@ public class BaseBall {
         }
     }
 }
+
+
+

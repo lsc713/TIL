bee32a982fcf195f9b06ffb85b5f6eb599775dd0
VanMao <139448668+lsc713@users.noreply.github.com>
2024-02-22 10:30:14 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.03 ms, Memory: 71.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        // int answer = 0;
+        // if (num1 == num2) {
+        //     return 1;
+        // } else {
+        //     return -1;
+        // }
+        
+        int answer = 0;
+        return num1 == num2 ? 1: -1;
+    }
+}

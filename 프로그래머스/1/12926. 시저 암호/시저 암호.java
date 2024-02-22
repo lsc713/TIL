51ba2d388a793fd2b933faeca6fa41d77c212e2c
VanMao <139448668+lsc713@users.noreply.github.com>
2024-02-22 23:37:05 +0900

    [level 1] Title: 시저 암호, Time: 1.62 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -1,16 +1,33 @@
 class Solution {
-    public String solution(String s, int n) {
-        String answer = "";
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
+    // public String solution(String s, int n) {
+//         String answer = "";
+//         for (int i = 0; i < s.length(); i++) {
+//             char ch = s.charAt(i);
 
-            if (Character.isLowerCase(ch)) {
-                ch = (char) ((ch - 'a' + n) % 26 + 'a');
-            } else if (Character.isUpperCase(ch)) {
-                ch = (char) ((ch - 'A' + n) % 26 + 'A');
-            }
-            answer+=ch;
+//             if (Character.isLowerCase(ch)) {
+//                 ch = (char) ((ch - 'a' + n) % 26 + 'a');
+//             } else if (Character.isUpperCase(ch)) {
+//                 ch = (char) ((ch - 'A' + n) % 26 + 'A');
+//             }
+//             answer+=ch;
+//         }
+//         return answer;
+    // }
+    
+    private char push(char c, int n){
+            if (!Character.isAlphabetic(c))return c;
+
+            int offset = Character.isUpperCase(c) ? 'A' : 'a';
+            int position = c-offset;
+            position = (position+n) % ('Z'-'A'+1);
+            return (char) (offset + position);
         }
-        return answer;
-    }
+
+        public String solution(String s, int n) {
+            StringBuilder sb = new StringBuilder();
+            for (char c : s.toCharArray()){
+                sb.append(push(c, n));
+            }
+            return sb.toString();
+        }
 }

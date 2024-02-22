41050c5b92109a572c8cf054db21251b8cf993b8
VanMao <139448668+lsc713@users.noreply.github.com>
2024-02-22 10:45:04 +0900

    [level 1] Title: 평균 구하기, Time: 1.88 ms, Memory: 71.1 MB -BaekjoonHub

--- 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
@@ -1,11 +1,15 @@
-    class Solution {
+import java.util.*    ;
+class Solution {
         public double solution(int[] arr) {
+            // double answer = 0;
+            // double sum = 0;
+            // for (int i = 0; i < arr.length; i++) {
+            //     sum += arr[i];
+            // }
+            // answer = sum/ arr.length;
+            // return answer;
             double answer = 0;
-            double sum = 0;
-            for (int i = 0; i < arr.length; i++) {
-                sum += arr[i];
-            }
-            answer = sum/ arr.length;
+            answer = Arrays.stream(arr).average().orElse(0);
             return answer;
         }
     }

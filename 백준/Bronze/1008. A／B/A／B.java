43ad6deb68e7d7a6ca34604366ec4851b77a1c2c
VanMao <139448668+lsc713@users.noreply.github.com>
2024-02-23 23:31:28 +0900

    [Bronze V] Title: A/B, Time: 208 ms, Memory: 17800 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main {
+    
+    
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextInt();
+        
+double b = sc.nextInt();
+    double sum = a/b;
+    System.out.println(sum);}
+}

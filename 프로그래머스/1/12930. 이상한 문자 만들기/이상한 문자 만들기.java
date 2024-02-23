ba88fbfb26720f30a38b61d3ca4325cc0a45c485
VanMao <139448668+lsc713@users.noreply.github.com>
2024-02-23 09:33:50 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.16 ms, Memory: 79.3 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -1,13 +1,31 @@
 class Solution {
     public String solution(String s) {
-        String answer = "";
-        int cnt = 0;
-        String[] arr = s.split("");
+//         String answer = "";
+//         int cnt = 0;
+//         String[] arr = s.split("");
 
-        for (String string : arr) {
-            cnt = string.contains(" ") ? 0 : cnt + 1;
-            answer += cnt % 2 == 0 ? string.toLowerCase() : string.toUpperCase();
-        }
-       return answer;
+//         for (String string : arr) {
+//             cnt = string.contains(" ") ? 0 : cnt + 1;
+//             answer += cnt % 2 == 0 ? string.toLowerCase() : string.toUpperCase();
+//         }
+//        return answer;
+        
+            StringBuilder sb = new StringBuilder();
+            boolean toUpper = true;
+
+            for(char c : s.toCharArray()){
+                if (!Character.isAlphabetic(c)) {
+                    sb.append(c);
+                    toUpper = true;
+                } else {
+                    if (toUpper) {
+                        sb.append(Character.toUpperCase(c));
+                    } else {
+                        sb.append(Character.toLowerCase(c));
+                    }
+                    toUpper = !toUpper;
+                }
+            }
+            return sb.toString();
     }
 }

1e6272c6bd4f1e77f1a0d11810f3e7296dffa3e8
이주호 <139448668+lsc713@users.noreply.github.com>
2024-04-15 11:49:39 +0900

    [Silver IV] Title: 설탕 배달, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -1,14 +1,14 @@
-n=int(input())
-count =0
-while(n>=0):
+n = int(input())
+result = 0
+while n >= 0:
     if n%5==0:
-        count+=n//5
-        print(count)
+        result += (n//5)
+        print(result)
         break
     n-=3
-    count+=1
+    result+=1
 else:
-    print(-1)
+    result = -1
+    print(result)
 
-
-
+    

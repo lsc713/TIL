77a9c6886a5b2c6db691f79c8dc6060cd53fb573
이주호 <139448668+lsc713@users.noreply.github.com>
2024-05-13 16:15:48 +0900

    Time: 61 ms (95.27%), Space: 15.7 MB (66.25%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        anagrams = collections.defaultdict(list)
+        for word in strs:
+            anagrams[''.join(sorted(word))].append(word)
+        return list(anagrams.values())
+        

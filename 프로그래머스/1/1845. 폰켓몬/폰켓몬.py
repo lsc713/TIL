0ec71261ca04ff4a84eeb8917d9fc7dc5e475c50
이주호 <139448668+lsc713@users.noreply.github.com>
2024-05-15 23:59:37 +0900

    [level 1] Title: 폰켓몬, Time: 0.21 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,7 +1,2 @@
 def solution(nums):
-    answer = 0
-    uniqueP=set(nums)
-    maxSel =len(nums)//2
-    answer = min(maxSel,len(uniqueP))
-        
-    return answer
+    return min(len(nums)//2,len(set(nums)))

40f0562406d7b9099915a42c16b2d4ea4966b730
이주호 <139448668+lsc713@users.noreply.github.com>
2024-05-18 23:36:02 +0900

    [Bronze I] Title: 소인수분해, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.py
+++ 백준/Bronze/11653. 소인수분해/소인수분해.py
@@ -1,9 +1,12 @@
 import sys
 input = sys.stdin.readline
 n=int(input())
-while n != 1:
-    for i in range(2,10000000):
-        if (n%i==0):
-            n=n//i
-            print(i)
-            break
+i=2
+while i*i<=n:
+    while n%i == 0:
+            
+        n//=i
+        print(i)
+    i+=1
+if n>1:
+    print(n)

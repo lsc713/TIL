1c3fb6018f7d4bfd771f4c004a0a7069c9065770
이주호 <139448668+lsc713@users.noreply.github.com>
2024-05-19 13:44:36 +0900

    [Platinum IV] Title: 공약수열, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Platinum/14252. 공약수열/공약수열.py
@@ -0,0 +1,27 @@
+import sys
+input = sys.stdin.readline
+n=int(input())
+arr = sorted(list(map(int,input().split())))
+def gcd(a,b):
+    while a%b != 0:
+        tmp = a%b
+        a=b
+        b=tmp
+    return b
+count=0
+arr2=[]
+for i in range(0,n-1):
+    if gcd(arr[i],arr[i+1])>1:
+        arr2.append([arr[i],arr[i+1]])
+for a,b in arr2:
+    for j in range(a+1,b):
+        tmp=0
+        if gcd(a,j) == 1: tmp+=1
+        if gcd(b,j) == 1: tmp+=1
+        if tmp>1:
+            count+=1
+            break
+        if j==b-1:
+            count+=2
+
+print(count)

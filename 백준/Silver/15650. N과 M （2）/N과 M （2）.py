a0175ddd7c45ef72f7a5116507b7eab216dceba2
이주호 <139448668+lsc713@users.noreply.github.com>
2024-05-20 23:34:22 +0900

    [Silver III] Title: N과 M (2), Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -1,18 +1,16 @@
-n, m = (map(int, input().split()))
-
-s = []
-
-
-def dfs(start):
-    if (len(s) == m):
-        print(' '.join(map(str, s)))
+n,m=map(int,input().split())
+visited=[0 for _ in range(n+1)]
+arr=[]
+def recur(start,number):
+    if number == m:
+        print(*arr)
         return
+    for i in range(start,n+1):
+        visited[i]=1
+        arr.append(i)
+        recur(i+1,number+1)
 
-    for i in range(start, n + 1):
-        if i not in s:
-            s.append(i)
-            dfs(i+1)
-            s.pop()
+        arr.pop()
+        visited[i]=0
 
-
-dfs(1);
+recur(1,0)

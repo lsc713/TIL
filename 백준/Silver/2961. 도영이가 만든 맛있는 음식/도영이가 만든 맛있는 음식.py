9885cf611050b1574f749d9f7b9dd52a319f801a
이주호 <139448668+lsc713@users.noreply.github.com>
2024-05-21 18:35:02 +0900

    [Silver II] Title: 도영이가 만든 맛있는 음식, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.py
+++ 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.py
@@ -1,24 +1,21 @@
 import sys
-
 input = sys.stdin.readline
 
-
-def recur(idx, sin, sun, use):
+def recur(idx,sin,sun,use):
     global answer
-    if idx == n:
-        if use > 0:
-            answer = min(answer, abs(sin - sun))
+    if idx==n:
+        if use>0:
+            answer=min(answer,abs(sin-sun))
         return
-    recur(idx + 1, sin * graph[idx][0], sun + graph[idx][1], use + 1)
-    recur(idx + 1, sin, sun, use)
+    recur(idx+1,sin * graph[idx][0], sun + graph[idx][1], use+1)
+    recur(idx+1,sin,sun,use)
 
-
-n = int(input())
-graph = [() for i in range(n)]
+n=int(input())
+graph=[() for _ in range(n)]
 for i in range(n):
-    a, b = map(int, input().split())
-    graph[i]=(a, b)
-answer = 1e9
-
-recur(0, 1, 0, 0)
+    a,b = map(int,input().split())
+    graph[i]=(a,b)
+    
+answer=1e9
+recur(0,1,0,0)
 print(answer)

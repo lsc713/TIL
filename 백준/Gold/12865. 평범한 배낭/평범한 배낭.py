811a50cf5cc8e02b410d801e66b5ec9d58f15077
이주호 <139448668+lsc713@users.noreply.github.com>
2024-05-22 11:47:11 +0900

    [Gold V] Title: 평범한 배낭, Time: 1960 ms, Memory: 155596 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,13 +1,23 @@
-n,k=map(int,input().split())
+import sys
 
-bag=[list(map(int,input().split()))for _ in range(n)]
-dp=[[0]*(k+1) for _ in range(n+1)]
+input = sys.stdin.readline
 
-for i in range(1,n+1):
-    for j in range(1,k+1):
-        if j >= bag[i-1][0]:
-            dp[i][j]=max(bag[i-1][1]+dp[i-1][j-bag[i-1][0]],dp[i-1][j])
-        else:
-            dp[i][j]=dp[i-1][j]
+n, k = map(int, input().split())
 
-print(dp[n][k])          
+
+def solution(idx, weight):
+    global answer
+    if weight > k:
+        return -9999999
+    if idx == n:
+        return 0
+    if dp[idx][weight] != -1:
+        return dp[idx][weight]
+    dp[idx][weight] = max(solution(idx + 1, weight+graph[idx][0]) + graph[idx][1], solution(idx+1, weight))
+    return dp[idx][weight]
+
+
+graph = [list(map(int,input().split())) for _ in range(n)]
+dp = [[-1 for _ in range(100001)] for i in range(n)]
+answer = solution(0,0)
+print(answer)

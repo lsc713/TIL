d2c25f75756b6b46be06271cfb8c54d8a253bc74
이주호 <139448668+lsc713@users.noreply.github.com>
2024-05-27 00:16:48 +0900

    [Gold V] Title: 전깃줄, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/2565. 전깃줄/전깃줄.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+    
+n=int(input())
+arr=sorted([list(map(int, input().split())) for _ in range(n)], key=lambda x:x[0])
+dp=[1]*(n+1)
+for i in range(n):
+    for j in range(i):
+        if arr[i][1] > arr[j][1]:
+            dp[i]=max(dp[i],dp[j]+1)
+
+
+print(n-max(dp))

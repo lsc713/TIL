484ec3020ae865bdc7ac34d935f56d03a313729f
이주호 <139448668+lsc713@users.noreply.github.com>
2024-05-30 14:03:27 +0900

    [Gold IV] Title: 최단경로, Time: 576 ms, Memory: 153600 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.py
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -1,33 +1,26 @@
-import sys
 import heapq
-input = sys.stdin.readline
-INF=int(1e9)
-
-def dijkstra(start):
-    q=[]
-    heapq.heappush(q,(0,start))
-    distance[start]=0
-    while q:
-        dist,now=heapq.heappop(q)
-        if distance[now]<dist:
-            continue
-        for i in graph[now]:
-            cost = dist+i[1]
-            if cost<distance[i[0]]:
-                distance[i[0]]=cost
-                heapq.heappush(q,(cost,i[0]))
-
-V,E = map(int,input().split())
-K=int(input())
-graph=[[]for _ in range(V+1)]
-distance=[INF]*(V+1)
-for i in range(E):
-    u,v,w=map(int,input().split())
-    graph[u].append((v,w))
-dijkstra(K)
-
-for i in range(1,V+1):
-    if distance[i]==INF:
+import sys
+input=sys.stdin.readline
+n,m=map(int,input().split())
+start = int(input())
+links =[[]for _ in range(n+1)]
+dist =[1e9 for _ in range(n+1)]
+for _ in range(m):
+    a,b,c=map(int,input().split())
+    links[a].append([b,c])
+q=[]
+heapq.heappush(q,[0,start])
+dist[start]=0
+while q:
+    _w,node = heapq.heappop(q)
+    if dist[node]<_w:
+        continue
+    for nxt,weight in links[node]:
+        if dist[node]+weight<dist[nxt]:
+            dist[nxt] = dist[node]+weight
+            heapq.heappush(q,[dist[nxt],nxt])
+for j in range(1,n+1):
+    if dist[j]==1e9:
         print("INF")
     else:
-        print(distance[i])
+        print(dist[j])

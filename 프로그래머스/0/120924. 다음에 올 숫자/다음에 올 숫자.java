1a7718582559a59bb22eb7b77bd28391908c5545
이주호 <139448668+lsc713@users.noreply.github.com>
2024-08-03 16:07:59 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.02 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int[] common) {
+        int answer = 0;
+        int cha = 0;
+        int bi = 0;
+        if (common[common.length-1]-common[common.length-2]==common[common.length-2]-common[common.length-3]){
+            answer = common[common.length-1]+common[common.length-1]-common[common.length-2];
+        }else{
+            answer = common[common.length-1]*(common[common.length-1]/common[common.length-2]);
+        }
+        return answer;
+    }
+}

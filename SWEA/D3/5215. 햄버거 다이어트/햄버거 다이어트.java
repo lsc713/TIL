e471fbaa602757ece9d8ec5c1fd5d5f4485f8538
이주호 <139448668+lsc713@users.noreply.github.com>
2024-08-19 22:20:40 +0900

    [D3] Title: 햄버거 다이어트, Time: 182 ms, Memory: 20,568 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -1,53 +1,53 @@
-import java.util.*;
-import java.io.FileInputStream;
 
+import java.util.*;
+import java.io.*;
 
 class Solution
 {
-    static int n;
-    static int limit;
-    static int[] score;
-    static int[] cal;
-    static int answer;
-    public static void main(String args[]) throws Exception
-    {
+static int n ;
+static int limit;
+static int[] score;
+static int[] cal;
+static int answer;
+	public static void main(String args[]) throws Exception
+	{
+BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		//Scanner sc = new Scanner(System.in);
+		int T;
+		T=Integer.parseInt(br.readLine());
+        StringTokenizer st;
 
-        Scanner sc = new Scanner(System.in);
-        int T;
-        T=sc.nextInt();
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+            st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		limit = Integer.parseInt(st.nextToken());
+		score = new int[n];
+		cal = new int[n];
+		for(int i = 0 ; i < n ; i++){
+            st = new StringTokenizer(br.readLine());
+			score[i]=Integer.parseInt(st.nextToken());
+			cal[i]=Integer.parseInt(st.nextToken());
+		}
+            answer=0;
+		recur(0,0,0);
+		StringBuilder sb = new StringBuilder();
+		sb.append("#"+test_case+" "+answer);
+		System.out.println(sb);
 
-
-        for(int test_case = 1; test_case <= T; test_case++)
-        {
-            answer =0;
-            StringBuilder sb = new StringBuilder();
-            
-			n = sc.nextInt();
-            limit = sc.nextInt();
-            score = new int[n];
-            cal = new int[n];
-            for(int i = 0 ; i < n ; i ++){
-                score[i] = sc.nextInt();
-                cal[i] = sc.nextInt();
-            }
-            recur(0,0,0);
-
-            sb.append("#"+test_case+" "+answer);
-            System.out.println(sb);
-
-        }
-    }
-    
-    static void recur(int depth, int scoreSum, int calSum){
-    	if(calSum>limit){
-        	return;
-        }
-        if(depth == n){
-        	if(calSum <= limit){
-            	answer= Math.max(answer,scoreSum);
-            }return;
-        }
-        recur(depth+1,scoreSum,calSum);
-        recur(depth+1,scoreSum+score[depth],calSum+cal[depth]);
-    }
+		}
+	}
+	static void recur(int depth,int scoreSum, int calSum){
+	if(calSum > limit){
+		return;
+	}
+	if(depth==n){
+		if(calSum <= limit){
+			answer = Math.max(answer,scoreSum);
+		}
+		return;
+	}
+		recur(depth+1,scoreSum,calSum);
+		recur(depth+1,scoreSum+score[depth],calSum+cal[depth]);
+	}
 }

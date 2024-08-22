2bc7bf92027bb1e4ee0e3c6bb71f5bd2e0c0147c
이주호 <139448668+lsc713@users.noreply.github.com>
2024-08-22 09:31:21 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 184 ms, Memory: 17940 KB -BaekjoonHub

---
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+class Main{
+    
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        char[][] board1 = new char[8][8];
+        char[][] board2 = new char[8][8];
+        int answer=Integer.MAX_VALUE;
+        //BWBWBWBW WBWBWBWB
+        sc.nextLine();
+        char[][] map = new char[n][m];
+        for(int i = 0; i < n ;i++){
+            String input = sc.nextLine();
+            map[i] = input.toCharArray();
+        }
+        
+        for(int i = 0 ; i <8;i++){
+            if(i%2==0){
+                board1[i] = "BWBWBWBW".toCharArray();
+                board2[i] = "WBWBWBWB".toCharArray();
+            }else{
+                board1[i] = "WBWBWBWB".toCharArray();
+                board2[i] = "BWBWBWBW".toCharArray();
+            }
+        }
+        
+        //맵을 돌면서 계산하고 최소 값을 반환하자
+        for(int i = 0 ; i <n-7;i++){
+            for(int j = 0 ; j<m-7;j++){
+            	int min1 = 0;
+                int min2 = 0;
+                for(int a=i;a<i+8;a++){
+                    for(int b=j;b<j+8;b++){
+                        if(map[a][b] != board1[a%8][b%8]){
+                        	min1++;
+                        }
+                        if(map[a][b] != board2[a%8][b%8]){
+                        	min2++;
+                        }
+                    }    
+                }
+                answer=Math.min(answer, Math.min(min1, min2));
+                
+            }
+        }
+        
+        System.out.println(answer);
+        
+    }
+}

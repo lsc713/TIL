c4e160fdb38032854f647b47fd286c1b4709d5ca
이주호 <139448668+lsc713@users.noreply.github.com>
2024-08-23 22:05:21 +0900

    [Silver III] Title: N과 M (1), Time: 2100 ms, Memory: 80388 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,33 +1,36 @@
 import java.util.*;
-import java.io.*;
-
-public class Main{
-    public static int[] arr;
-    public static boolean[] visit;
+class Main{
+    static int n;
+    static int m;
+    static List<Integer> arr;
+    static boolean[] visited;
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        arr = new int[m];
-        visit = new boolean[n];
-        dfs(n,m,0);
-        }
-
-    public static void dfs(int n , int m , int depth){
-        if(depth==m){
-            for(int val : arr){
-                System.out.print(val+" ");
+        n = sc.nextInt();
+        m = sc.nextInt();
+        visited = new boolean[n+1];
+        arr = new ArrayList<>();
+        recur(0);
+        
+    }
+    static void recur(int idx){
+        if(idx==m){
+            for(int i =0;i<arr.size();i++){
+                System.out.print(arr.get(i)+" ");
             }
             System.out.println();
             return;
         }
-        for(int i = 0; i<n;i++){
-            if(!visit[i]){
-                visit[i]=true;
-                arr[depth]=i+1;
-                dfs(n,m,depth+1);
-                visit[i]=false;
+        for(int i = 1 ; i<=n;i++){
+            if(visited[i]){
+                continue;
             }
-    }
+            visited[i]=true;
+            arr.add(i);
+            recur(idx+1);
+            arr.remove(arr.size()-1);
+            visited[i]=false;    
         }
+        
+    }
 }

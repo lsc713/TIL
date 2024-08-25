a1eba88533568faaebb0e19d01ec8c832679b78c
이주호 <139448668+lsc713@users.noreply.github.com>
2024-08-25 23:22:45 +0900

    [D3] Title: [S/W 문제해결 기본] 8일차 - 암호문2, Time: 186 ms, Memory: 27,884 KB -BaekjoonHub

--- SWEA/D3/1229. ［S／W 문제해결 기본］ 8일차 － 암호문2/［S／W 문제해결 기본］ 8일차 － 암호문2.java
+++ SWEA/D3/1229. ［S／W 문제해결 기본］ 8일차 － 암호문2/［S／W 문제해결 기본］ 8일차 － 암호문2.java
@@ -3,49 +3,40 @@ import java.io.FileInputStream;
 
 class Solution
 {
-	public static void main(String args[]) throws Exception
-	{
-		
-		Scanner sc = new Scanner(System.in);
-//		Scanner sc = new Scanner(new FileInputStream("input (2).txt"));
-		
-	
+    public static void main(String args[]) throws Exception
+    {
 
-		for(int test_case = 1; test_case <= 10; test_case++)
-		{
-            StringBuilder sb = new StringBuilder();
-			String answer="";
-			int n = sc.nextInt();
-			List<Integer> arr = new ArrayList<>();
-			for(int i = 0 ; i<n;i++) {
-				arr.add(sc.nextInt());	
-			}
-			int m = sc.nextInt();
-			sc.nextLine();
-			String str = sc.nextLine();
-			
-			String[] strArr = str.split(" ");
-			for(int i  = 0 ; i<strArr.length;i++) {
-				if(strArr[i].equals("I")) {
-					for(int j = 0 ; j<Integer.parseInt(strArr[i+2]);j++) {
-//						System.out.println(strArr[i+3+Integer.parseInt(strArr[i+2])-1-j]);
-						arr.add(Integer.parseInt(strArr[i+1]),Integer.parseInt(strArr[i+3+Integer.parseInt(strArr[i+2])-1-j]));
-					}
-				}else if(strArr[i].equals("D")) {
-					for(int j = 0 ; j<Integer.parseInt(strArr[i+2]);j++) {
-//						System.out.println(strArr[i+1]);
-						arr.remove(Integer.parseInt(strArr[i+1]));
-					}
-				}else {
-					continue;
-				}
-			}
-			
-			for(int i = 0 ; i <10;i++) {
-				answer+=arr.get(i)+" ";
-			}
-			sb.append("#"+test_case+" "+answer.trim());
-            System.out.println(sb);
-		}
-	}
+        Scanner sc = new Scanner(System.in);
+        for(int test_case = 1; test_case <= 10; test_case++)
+        {
+            int n = sc.nextInt();
+            sc.nextLine();
+            String input = sc.nextLine();
+            List<String> strArr = new ArrayList<>();
+            for (int i = 0; i < n; i++) {
+                strArr.add(input.split(" ")[i]);
+            }
+
+            int m = sc.nextInt();
+            sc.nextLine();
+            String add = sc.nextLine();
+            String[] addArr = add.split(" ");
+            for (int i = 0; i < addArr.length; i++) {
+                if (addArr[i].equals("I")) {
+                    for (int j = i+2+Integer.parseInt(addArr[i+2]); j >= i+3 ; j--) {
+                        strArr.add(Integer.parseInt(addArr[i+1]),addArr[j]);
+                    }
+                }else if (addArr[i].equals("D")) {
+                    for (int j = 0; j < Integer.parseInt(addArr[i+2]) ; j++) {
+                        strArr.remove(Integer.parseInt(addArr[i+1]));
+                    }
+                }
+            }
+            String answer = "";
+            for (int i = 0; i < 10; i++) {
+                answer+=strArr.get(i)+" ";
+            }
+            System.out.println("#"+test_case+" "+ answer);
+        }
+    }
 }

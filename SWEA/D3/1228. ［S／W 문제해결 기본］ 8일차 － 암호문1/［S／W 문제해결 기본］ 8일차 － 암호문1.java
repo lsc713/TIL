bcac5379b253a5eba1514cc52c9047ee7bfaae20
이주호 <139448668+lsc713@users.noreply.github.com>
2024-08-25 23:17:40 +0900

    [D3] Title: [S/W 문제해결 기본] 8일차 - 암호문1, Time: 167 ms, Memory: 21,148 KB -BaekjoonHub

--- SWEA/D3/1228. ［S／W 문제해결 기본］ 8일차 － 암호문1/［S／W 문제해결 기본］ 8일차 － 암호문1.java
+++ SWEA/D3/1228. ［S／W 문제해결 기본］ 8일차 － 암호문1/［S／W 문제해결 기본］ 8일차 － 암호문1.java
@@ -1,48 +1,70 @@
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
+// 아래 표준 입출력 예제 필요시 참고하세요.
+// 표준 입력 예제
+// int a;
+// double b;
+// char g;
+// String var;
+// long AB;
+// a = sc.nextInt();                           // int 변수 1개 입력받는 예제
+// b = sc.nextDouble();                        // double 변수 1개 입력받는 예제
+// g = sc.nextByte();                          // char 변수 1개 입력받는 예제
+// var = sc.next();                            // 문자열 1개 입력받는 예제
+// AB = sc.nextLong();                         // long 변수 1개 입력받는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 표준 출력 예제
+// int a = 0;                            
+// double b = 1.0;               
+// char g = 'b';
+// String var = "ABCDEFG";
+// long AB = 12345678901234567L;
+//System.out.println(a);                       // int 변수 1개 출력하는 예제
+//System.out.println(b); 		       						 // double 변수 1개 출력하는 예제
+//System.out.println(g);		       						 // char 변수 1개 출력하는 예제
+//System.out.println(var);		       				   // 문자열 1개 출력하는 예제
+//System.out.println(AB);		       				     // long 변수 1개 출력하는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
 import java.util.*;
 import java.io.FileInputStream;
 
+/*
+   사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
+   이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
+ */
 class Solution
 {
 	public static void main(String args[]) throws Exception
-	{
-		
-		Scanner sc = new Scanner(System.in);
-		
-	
+    {
 
-		for(int test_case = 1; test_case <= 10; test_case++)
-		{
-            StringBuilder sb = new StringBuilder();
-			String answer="";
-			int n = sc.nextInt();
-			List<Integer> arr = new ArrayList<>();
-			for(int i = 0 ; i<n;i++) {
-				arr.add(sc.nextInt());	
-			}
-			int m = sc.nextInt();
-			sc.nextLine();
-			String str = sc.nextLine();
-			String[] strArr = str.split("I");
-//			System.out.println(arr);
-			for(int i = 0 ; i <strArr.length;i++) {
-				strArr[i]=strArr[i].trim();
-				if(strArr[i].equals("")) {
-					continue;
-				}
-				String[] tempArr = strArr[i].split(" ");
-				if(Integer.parseInt(tempArr[0])>=10) {
-					continue;
-				}
-//				System.out.println(Arrays.toString(tempArr));
-				for(int j = Integer.parseInt(tempArr[1])+1 ; j>=2;j--) {
-					arr.add(Integer.parseInt(tempArr[0]),Integer.parseInt(tempArr[j]));
-				}
-			}
-			for(int i = 0 ; i <10;i++) {
-				answer+=arr.get(i)+" ";
-			}
-			sb.append("#"+test_case+" "+answer.trim());
-            System.out.println(sb);
-		}
-	}
+        Scanner sc = new Scanner(System.in);
+        for(int test_case = 1; test_case <= 10; test_case++)
+        {
+            int n = sc.nextInt();
+            sc.nextLine();
+            String input = sc.nextLine();
+            List<String> strArr = new ArrayList<>();
+            String[] tmp = input.split(" ");
+            for(int i =0;i<tmp.length;i++){
+            	strArr.add(tmp[i]);
+            }
+
+            int m = sc.nextInt();
+            sc.nextLine();
+            String add = sc.nextLine();
+            String[] addArr = add.split(" ");
+            for (int i = 0; i < addArr.length; i++) {
+                if (addArr[i].equals("I")) {
+                    for (int j = i+2+Integer.parseInt(addArr[i+2]); j >= i+3 ; j--) {
+                        strArr.add(Integer.parseInt(addArr[i+1]),addArr[j]);
+                    }
+                }
+            }
+            String answer = "";
+            for (int i = 0; i < 10; i++) {
+                answer+=strArr.get(i)+" ";
+            }
+            System.out.println("#"+test_case+" "+ answer);
+        }
+    }
 }

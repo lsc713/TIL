8b05569accbe43a9db77b32fac247c90a4c58b48
이주호 <139448668+lsc713@users.noreply.github.com>
2024-09-04 11:45:48 +0900

    [D4] Title: 창용 마을 무리의 개수, Time: 207 ms, Memory: 46,120 KB -BaekjoonHub

--- SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.java
+++ SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.java
@@ -1,54 +1,50 @@
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
+import java.io.FileInputStream;
 
 class Solution
 {
-    static int n ;
+    static int n;
     static int m;
-    static List<Integer>[] list;
-    static boolean[] visited;
-    public static void main(String args[]) throws Exception
-    {
+    static List<Integer>[] arr;
+    static boolean visited[];
+    static int answer;
+	public static void main(String args[]) throws Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T=sc.nextInt();
 
-        Scanner sc = new Scanner(System.in);
-        int T;
-        T=sc.nextInt();
-
-
-        for(int test_case = 1; test_case <= T; test_case++)
-        {
-            n = sc.nextInt(); 
-            m = sc.nextInt(); 
-            list = new ArrayList[n+1]; 
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+			n=sc.nextInt();
+            m =sc.nextInt();
+            answer=0;
+			arr = new ArrayList[n+1];
             visited = new boolean[n+1];
-            for (int i = 1; i <= n; i++) {
-                list[i] = new ArrayList<>(); 
+            
+            for(int i = 1;i<=n;i++){
+            	arr[i] = new ArrayList<>();
             }
-            for (int i = 0; i < m; i++) {
+            for(int i = 0 ; i < m;i++){
                 int a = sc.nextInt();
-                int b = sc.nextInt();
-                list[a].add(b); 
-                list[b].add(a);
+                int b =sc.nextInt();
+            	arr[a].add(b);
+                arr[b].add(a);
             }
-            int answer=0;
-
-            for (int i = 1; i <= n; i++) {
-                if (!visited[i]) {
-                    dfs(i);
+            for(int i =1;i<=n;i++){
+            	if(!visited[i]){
+                	dfs(i);
                     answer++;
                 }
             }
             System.out.println("#"+test_case+" "+answer);
-        }
-    }
-
-    static void dfs(int depth) {
-        visited[depth] = true;
-        for (int relation : list[depth]) {
-            if (!visited[relation]) {
-                dfs(relation);
+		}
+	}
+    static void dfs(int idx){
+    	visited[idx]=true;
+        for(int val : arr[idx]){
+        	if(!visited[val]){
+            	dfs(val);
             }
         }
     }

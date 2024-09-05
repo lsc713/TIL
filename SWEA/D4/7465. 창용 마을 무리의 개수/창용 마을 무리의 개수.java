d7b30b187c9b7679ea64e10d3a80dce13fed2499
이주호 <139448668+lsc713@users.noreply.github.com>
2024-09-05 14:57:57 +0900

    [D4] Title: 창용 마을 무리의 개수, Time: 207 ms, Memory: 46,120 KB -BaekjoonHub

--- SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.java
+++ SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.java
@@ -1,51 +1,51 @@
-import java.util.*;
-import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
-class Solution
-{
-    static int n;
-    static int m;
-    static List<Integer>[] arr;
-    static boolean visited[];
-    static int answer;
-	public static void main(String args[]) throws Exception
-	{
+public class Solution {
+	static int[] parent;
+	static int n;
+	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int T;
-		T=sc.nextInt();
-
-		for(int test_case = 1; test_case <= T; test_case++)
-		{
-			n=sc.nextInt();
-            m =sc.nextInt();
-            answer=0;
-			arr = new ArrayList[n+1];
-            visited = new boolean[n+1];
-            
-            for(int i = 1;i<=n;i++){
-            	arr[i] = new ArrayList<>();
-            }
-            for(int i = 0 ; i < m;i++){
-                int a = sc.nextInt();
-                int b =sc.nextInt();
-            	arr[a].add(b);
-                arr[b].add(a);
-            }
-            for(int i =1;i<=n;i++){
-            	if(!visited[i]){
-                	dfs(i);
-                    answer++;
-                }
-            }
-            System.out.println("#"+test_case+" "+answer);
+		int T = sc.nextInt();
+		for(int tc =1;tc<=T;tc++) {
+			n = sc.nextInt();
+			int m =sc.nextInt();
+			
+			parent = new int[n+1];
+			makeSet();
+			
+			for(int i = 0 ; i<m;i++) {
+				int a =sc.nextInt();
+				int b =sc.nextInt();
+				
+				if(findSet(a)!=findSet(b)) {
+					union(a,b);
+				}
+			}
+			int answer=0;
+			for(int i = 1 ; i <=n;i++) {
+				if(findSet(i)==i) {
+					answer++;
+				}
+			}
+			System.out.println("#"+tc+" "+answer);
 		}
 	}
-    static void dfs(int idx){
-    	visited[idx]=true;
-        for(int val : arr[idx]){
-        	if(!visited[val]){
-            	dfs(val);
-            }
-        }
-    }
+	static void makeSet() {
+		for(int i = 1 ; i <=n;i++) {
+			parent[i] = i;//각 목의 주인은 본인
+		}
+	}
+	
+	static void union(int x,int y) {
+		parent[findSet(y)] = findSet(x);
+	}
+	
+	static int findSet(int x) {
+		if(x!=parent[x]) {
+			parent[x] = findSet(parent[x]);
+		}
+		return parent[x];
+	}
 }

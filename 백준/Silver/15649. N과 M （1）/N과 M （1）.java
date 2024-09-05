404669ed80a13f7b041d7b7f15b6814b2d35bb64
이주호 <139448668+lsc713@users.noreply.github.com>
2024-09-05 17:47:44 +0900

    [Silver III] Title: N과 M (1), Time: 1936 ms, Memory: 68708 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,36 +1,33 @@
 import java.util.*;
 class Main{
+    static boolean[] visited;
+    static int[] arr;
     static int n;
     static int m;
-    static List<Integer> arr;
-    static boolean[] visited;
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        m = sc.nextInt();
-        visited = new boolean[n+1];
-        arr = new ArrayList<>();
+        n =sc.nextInt();
+        m =sc.nextInt();
+        arr= new int[m];
+        visited = new boolean[n];
         recur(0);
-        
     }
-    static void recur(int idx){
-        if(idx==m){
-            for(int i =0;i<arr.size();i++){
-                System.out.print(arr.get(i)+" ");
+    static void recur(int depth){
+        if(depth ==m){
+            for(int val : arr){
+                System.out.print(val+" ");
             }
             System.out.println();
             return;
         }
-        for(int i = 1 ; i<=n;i++){
-            if(visited[i]){
-                continue;
+        for(int i = 0 ; i<n;i++){
+            if(!visited[i]){
+                visited[i]=true;
+                arr[depth]=i+1;
+                recur(depth+1);
+                visited[i]=false;
             }
-            visited[i]=true;
-            arr.add(i);
-            recur(idx+1);
-            arr.remove(arr.size()-1);
-            visited[i]=false;    
+            
         }
-        
     }
 }

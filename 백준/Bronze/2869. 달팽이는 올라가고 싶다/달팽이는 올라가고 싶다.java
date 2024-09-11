89faa68ec99a97a965b936300335b2640030b341
이주호 <139448668+lsc713@users.noreply.github.com>
2024-09-12 02:50:01 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 176 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        int V = scanner.nextInt();
+        System.out.println((int)Math.ceil((double)(V - B)/(A - B)));
+    }
+}

ddbd5b3a4f8571965d7bc2c06927660718c98c66
이주호 <139448668+lsc713@users.noreply.github.com>
2024-09-12 11:21:31 +0900

    [D3] Title: 햄버거 다이어트, Time: 182 ms, Memory: 20,568 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -1,53 +1,39 @@
-
-import java.util.*;
-import java.io.*;
+import java.util.Scanner;
 
 class Solution
 {
-static int n ;
-static int limit;
-static int[] score;
-static int[] cal;
-static int answer;
-	public static void main(String args[]) throws Exception
-	{
-BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		//Scanner sc = new Scanner(System.in);
-		int T;
-		T=Integer.parseInt(br.readLine());
-        StringTokenizer st;
-
-		for(int test_case = 1; test_case <= T; test_case++)
-		{
-            st = new StringTokenizer(br.readLine());
-		n = Integer.parseInt(st.nextToken());
-		limit = Integer.parseInt(st.nextToken());
-		score = new int[n];
-		cal = new int[n];
-		for(int i = 0 ; i < n ; i++){
-            st = new StringTokenizer(br.readLine());
-			score[i]=Integer.parseInt(st.nextToken());
-			cal[i]=Integer.parseInt(st.nextToken());
-		}
-            answer=0;
-		recur(0,0,0);
-		StringBuilder sb = new StringBuilder();
-		sb.append("#"+test_case+" "+answer);
-		System.out.println(sb);
-
-		}
-	}
-	static void recur(int depth,int scoreSum, int calSum){
-	if(calSum > limit){
-		return;
-	}
-	if(depth==n){
-		if(calSum <= limit){
-			answer = Math.max(answer,scoreSum);
-		}
-		return;
-	}
-		recur(depth+1,scoreSum,calSum);
-		recur(depth+1,scoreSum+score[depth],calSum+cal[depth]);
-	}
+	static int N,L,answer;
+	static int[] arr[];
+	static int[][] dp;
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for(int tc=1;tc<=T;tc++) {
+			N =sc.nextInt();//물건개수
+			answer=0;
+			L = sc.nextInt();//칼로리 한계
+			arr = new int[N+1][2];//각 음식의 맛과 칼로리
+			dp = new int[N+1][L+1];//메모이제이션을 위한 2차원 배열
+			for(int i = 1 ;i <= N;i++) {//각 물건에 대한
+				int V = sc.nextInt();//맛
+				int C= sc.nextInt();//칼로리
+				arr[i][1] = V;//맛
+				arr[i][0] = C;//칼로리
+			}
+			answer = 0;
+			
+			for(int i = 1;i<=N;i++) {
+				for(int j =1;j<=L;j++) {
+					if(arr[i][0] > j ) {//만약에 가방 무게보다 큰 무게를 갖는 물건인 경우
+						dp[i][j] = dp[i-1][j];//이전 물건의 내용을 기입.
+					}else {//가방에 들어갈 수 있는 무게의 물건이라면.. 이전 내용물과 
+						dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j-arr[i][0]]+arr[i][1]);
+					}
+				}
+			}
+			answer = dp[N][L];
+			System.out.println("#"+tc+" "+answer);
+		}//tc
+	}//main
+	
 }

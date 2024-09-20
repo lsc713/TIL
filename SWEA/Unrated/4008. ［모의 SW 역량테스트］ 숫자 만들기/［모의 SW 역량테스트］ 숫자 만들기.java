006386affe6e22843514945c5be854dbba01a12f
이주호 <139448668+lsc713@users.noreply.github.com>
2024-09-20 17:07:38 +0900

    [Unrated] Title: [모의 SW 역량테스트] 숫자 만들기, Time: 172 ms, Memory: 25,280 KB -BaekjoonHub

--- SWEA/Unrated/4008. ［모의 SW 역량테스트］ 숫자 만들기/［모의 SW 역량테스트］ 숫자 만들기.java
+++ SWEA/Unrated/4008. ［모의 SW 역량테스트］ 숫자 만들기/［모의 SW 역량테스트］ 숫자 만들기.java
@@ -1,78 +1,82 @@
-import java.util.Scanner;
-import java.io.FileInputStream;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 class Solution
 {
-    static int n;
-    static int max;
-    static int min;
-    static int[] arr;
-    static int[] operators;
-    static int[] result;
-    static int answer;
-	public static void main(String args[]) throws Exception
-	{
-
-		Scanner sc = new Scanner(System.in);
-		int T;
-		T=sc.nextInt();
-
-
-		for(int test_case = 1; test_case <= T; test_case++)
-		{
-
-            n =sc.nextInt();
-            arr = new int[n];
-            operators = new int[4];
-            result  = new int[arr.length-1];
-            max = Integer.MIN_VALUE;
-            min = Integer.MAX_VALUE;
-            for(int i = 0 ;i<4;i++){
-            	operators[i] = sc.nextInt();
-            }
-            for(int i = 0 ;i<n;i++){
-            	arr[i] = sc.nextInt();
-            }
-            
-            recur(0);
-            answer = max - min;
-            System.out.println("#"+test_case+" "+answer);
+	static int n,answer,max,min;
+	static int[] operators;
+	static int[] arr;
+	static int[] result;
+	public static void main(String[] args) throws NumberFormatException, IOException {
+//		Scanner sc = new Scanner(System.in);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int T = Integer.parseInt(br.readLine());
+		for(int t=1;t<=T;t++) {
+			StringBuilder sb = new StringBuilder();
+			 n = Integer.parseInt(br.readLine());
+			 answer=0;
+			 max=Integer.MIN_VALUE;
+			 min=Integer.MAX_VALUE;
+			 operators = new int[4];
+			 arr=new int[n];
+			 result=new int[arr.length-1];
+			 st = new StringTokenizer(br.readLine());
+			 for(int i = 0 ; i <4;i++) {
+				 operators[i]=Integer.parseInt(st.nextToken());
+			 }
+			 st = new StringTokenizer(br.readLine());
+			for(int i = 0 ; i < n ;i++) {
+				arr[i]=Integer.parseInt(st.nextToken());
+			}
+			
+			recur(0);
+			
+			answer=max-min;
+//			System.out.println(max+" "+min);
+			sb.append("#"+t+" "+answer);
+			System.out.println(sb);
+		}//tc
+	}//main
+	static void recur(int depth) {
+		if(depth==arr.length-1) {
+			cal();
+			return;
+		}
+		for(int i=0;i<4;i++) {
+			if(operators[i]>0) {
+				operators[i]--;
+				
+				result[depth]=i;
+				recur(depth+1);
+				operators[i]++;
+			}
 		}
 	}
-    static void recur(int depth){
-    	if(depth == result.length){
-            //연산
-            cal();
-        	return;
-        }
-        for(int i = 0 ;i<operators.length;i++){
-        	if(operators[i] >0){
-            	operators[i]--;
-                result[depth]=i;
-                recur(depth+1);
-                operators[i]++;
-            }
-        }
-    }
-    static void cal(){
-    	int num = arr[0];
-        for(int i = 0;i<result.length;i++){
-        	if(result[i] ==0){
-            	num+=arr[i+1];
-            }else if(result[i] ==1){
-            	num-=arr[i+1];
-            }else if(result[i] ==2){
-            	num*=arr[i+1];
-            }else if(result[i] ==3){
-            	num/=arr[i+1];
-            }
-        }
-        if(num > max){
-        	max = num;
-        }
-        if(num<min){
-        	min = num;
-        }
-    }
+	static void cal() {
+		int tmp = arr[0];
+//		System.out.println(Arrays.toString(result));
+		for(int i = 0 ; i<result.length;i++) {
+//			System.out.println(result[i] + " "+ tmp);
+			if(result[i]==0) {
+				tmp+=arr[i+1];
+			}else if(result[i]==1) {
+				tmp-=arr[i+1];
+			}else if(result[i]==2) {
+				tmp*=arr[i+1];
+			}else if(result[i]==3) {
+				tmp/=arr[i+1];
+			}
+		}
+//		System.out.println(tmp);
+		if(min>tmp) {
+			min = tmp;
+		}
+		if(max<tmp) {
+			max = tmp;
+		}
+	}
 }

e9810884ac3ecebba5c92d58462c723a72075cb9
이주호 <139448668+lsc713@users.noreply.github.com>
2024-09-20 16:26:05 +0900

    [D4] Title: 창용 마을 무리의 개수, Time: 216 ms, Memory: 42,016 KB -BaekjoonHub

--- SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.java
+++ SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.java
@@ -1,52 +1,43 @@
 import java.util.Scanner;
-import java.io.FileInputStream;
 
 class Solution
 {
-    static int answer;
-    static int[] arr;
-	public static void main(String args[]) throws Exception
-	{
-		Scanner sc = new Scanner(System.in);
-		int T;
-        
-		T=sc.nextInt();
-		
-		for(int test_case = 1; test_case <= T; test_case++)
-		{
-            answer=0;
-			int n =sc.nextInt();
-            arr = new int[n+1];
-            int m = sc.nextInt();
-            for(int i = 1;i<=n;i++){
-            	makeSet(i);
-            }
-            for(int i = 0 ;i<m;i++){
-            	int a = sc.nextInt();
-                int b= sc.nextInt();
-                if(findSet(a) != findSet(b)){
-                	union(a,b);
-                }
-            }
-            for(int i =1;i<=n;i++){
-            	if(arr[i] == i){
-                answer++;
-                }
-            }
-            
-            System.out.println("#"+test_case+" "+answer);
-		}
+	static int n,m,answer;
+	static int[] arr;
+	
+	public static void main(String[] args) {
+		Scanner sc =new Scanner(System.in);
+		int T = sc.nextInt();
+		for(int t=1;t<=T;t++) {
+			n=sc.nextInt();
+			m=sc.nextInt();
+			answer=0;
+			arr=new int[n+1];
+			for(int i =1;i<=n;i++) {
+				makeSet(i);
+			}
+			for(int i = 0 ; i <m;i++) {
+				int a = sc.nextInt();
+				int b = sc.nextInt();
+				
+				union(a,b);
+			}
+			for(int i = 1 ; i <=n;i++) {
+				if(arr[i] == i)answer++;
+			}
+			System.out.println("#"+t+" "+answer);
+		}//tc
+	}//main
+	static void makeSet(int i) {
+		arr[i] = i;
 	}
-    static int findSet(int x){
-    	if(x!=arr[x]){
-        	arr[x] = findSet(arr[x]);
-        }
-        return arr[x];
-    }
-    static void union(int x,int y){
-    	arr[findSet(y)] = findSet(x);
-    }
-    static void makeSet(int x){
-    	arr[x] = x;
-    }
+	static int findSet(int x) {
+		if(x!=arr[x]) {
+			arr[x] = findSet(arr[x]);
+		}
+		return arr[x];
+	}
+	static void union(int x,int y) {
+		arr[findSet(y)] = findSet(x);
+	}
 }

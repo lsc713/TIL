33d184488982534301db2a7afd51ece1f5a78cce
이주호 <139448668+lsc713@users.noreply.github.com>
2024-09-22 00:03:53 +0900

    [D3] Title: 0/1 Knapsack, Time: 188 ms, Memory: 33,172 KB -BaekjoonHub

--- SWEA/D3/3282. 0／1 Knapsack/0／1 Knapsack.java
+++ SWEA/D3/3282. 0／1 Knapsack/0／1 Knapsack.java
@@ -5,34 +5,32 @@ class Solution
 {
 	public static void main(String args[]) throws Exception
 	{
-
 		Scanner sc = new Scanner(System.in);
 		int T;
 		T=sc.nextInt();
 
 		for(int test_case = 1; test_case <= T; test_case++)
 		{
-		int n = sc.nextInt();
-            int k = sc.nextInt();
-            int[][] dp= new int[n+1][k+1];
-            int[][] arr = new int[n+1][2];
-            for(int i = 1;i<=n;i++){
-            int v = sc.nextInt();
-                int c= sc.nextInt();
+            int n =sc.nextInt();
+            int k =sc.nextInt();
+            int[][] dp = new int[n+1][k+1];
+            int[][] arr= new int[n+1][2];
+            for(int i=1;i<=n;i++){
+            	int v =sc.nextInt();
+                int c = sc.nextInt();
                 arr[i][0]=v;
                 arr[i][1]=c;
             }
-            for(int i = 1;i<=n;i++){
-            	for(int j =1;j<=k;j++){
+            for(int i =1;i<=n;i++){
+            	for(int j=1;j<=k;j++){
                 	if(arr[i][0]>j){
-                    	dp[i][j]=dp[i-1][j];
+                    	dp[i][j] = dp[i-1][j];
                     }else{
                     	dp[i][j]=Math.max(dp[i-1][j],dp[i-1][j-arr[i][0]]+arr[i][1]);
                     }
                 }
             }
-            int answer = dp[n][k];
-System.out.println("#"+test_case+" "+answer);
+            System.out.println("#"+test_case+" "+dp[n][k]);
 		}
 	}
 }

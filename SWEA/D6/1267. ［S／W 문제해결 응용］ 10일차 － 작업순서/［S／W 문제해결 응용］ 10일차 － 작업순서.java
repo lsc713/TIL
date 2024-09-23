bcce712f8306d759bb4007d6345cfe13db59bd74
이주호 <139448668+lsc713@users.noreply.github.com>
2024-09-23 12:46:32 +0900

    [D6] Title: [S/W 문제해결 응용] 10일차 - 작업순서, Time: 236 ms, Memory: 49,180 KB -BaekjoonHub

--- SWEA/D6/1267. ［S／W 문제해결 응용］ 10일차 － 작업순서/［S／W 문제해결 응용］ 10일차 － 작업순서.java
+++ SWEA/D6/1267. ［S／W 문제해결 응용］ 10일차 － 작업순서/［S／W 문제해결 응용］ 10일차 － 작업순서.java
@@ -1,51 +1,66 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
-import java.util.Stack;
 
-class Solution
-{
-	static int V,E;
-	static int[] adjArr[];
+public class Solution {
+//	static class Edges implements Comparable<Edges>{
+//		int A,B,W;
+//		public Edges(int A, int B,int W) {
+//			this.A = A;
+//			this.B = B;
+//			this.W = W;
+//			
+//		}
+//		@Override
+//		public int compareTo(Edges o) {
+//			return Integer.compare(this.W, o.W);
+//		}
+//	}
 	static boolean visited[];
-	static StringBuilder sb;
+	static ArrayList<Integer>[] arr;
 	static int[] degree;
+	static int V,E;
+	static String answer;
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		for(int tc=1;tc<=10;tc++) {
-			sb = new StringBuilder();
-			V = sc.nextInt();
-			E = sc.nextInt();
-			st= new Stack<>();
+		int T = 10;
+		for(int t = 1; t<=T;t++) {
+			V=sc.nextInt();
+			answer="";
 			visited = new boolean[V+1];
-			adjArr = new int[V+1][V+1];
+			E=sc.nextInt();
 			degree = new int[V+1];
-			for(int i = 0 ;i<E;i++) {
+			arr = new ArrayList[V+1];
+			for(int i =1;i<=V;i++) {
+				arr[i] = new ArrayList<>();
+			}
+			for(int i =1;i<=E;i++) {
 				int a = sc.nextInt();
 				int b = sc.nextInt();
-				adjArr[a][b]=1;
+				arr[a].add(b);
 				degree[b]++;
 			}
-			for(int i = 1;i<=V;i++) {
-				if(degree[i]==0) {
-					dfs(i);
+			for(int i=1;i<=V;i++) {
+				if(degree[i]==0&&!visited[i]) {
+					
+					answer+=i+" ";
+					
+					topol(i);
 				}
 			}
-			sb.append("#"+tc+" ");
-			while(!st.isEmpty()) {
-				sb.append(st.pop()+" ");
-			}
-			System.out.println(sb);
-		}
-		 
-	}
-	static Stack<Integer> st;
-	static void dfs(int curr) {
-		visited[curr] = true;
-		for(int i = 1;i<=V;i++) {
-			if(adjArr[curr][i]==1 && !visited[i]) {
-				dfs(i);
+			
+			System.out.println("#"+t+" "+answer);
+		}//tc
+	}//main
+	static void topol(int x) {
+		visited[x]=true;
+		for(int val : arr[x]) {
+			degree[val]--;
+			if(!visited[val]&&degree[val]==0) {
+				visited[val]=true;
+				answer+=val+" ";
+				topol(val);
 			}
 		}
-		st.push(curr);
 	}
-	
 }

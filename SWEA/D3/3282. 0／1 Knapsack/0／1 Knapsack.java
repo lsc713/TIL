b68a0df07973877f6e884e792ea94436316b3346
이주호 <139448668+lsc713@users.noreply.github.com>
2024-09-27 08:58:52 +0900

    [D3] Title: 0/1 Knapsack, Time: 188 ms, Memory: 33,172 KB -BaekjoonHub

--- SWEA/D3/3282. 0／1 Knapsack/0／1 Knapsack.java
+++ SWEA/D3/3282. 0／1 Knapsack/0／1 Knapsack.java
@@ -1,36 +1,37 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 import java.util.Scanner;
-import java.io.FileInputStream;
 
-class Solution
-{
-	public static void main(String args[]) throws Exception
-	{
+public class Solution {
+	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int T;
-		T=sc.nextInt();
+		int T = sc.nextInt();
+		for(int t=1;t<=T;t++) {
+			int n =sc.nextInt();//stuff count
+			int w = sc.nextInt();//bag weight
+			int[][] arr = new int[n+1][2];
+			for(int i = 1 ; i <= n ;i++) {
+				int weight = sc.nextInt();
+				int value = sc.nextInt();
+				arr[i][0]=weight;
+				arr[i][1]=value;
+			}
+			int[][] dp = new int[n+1][w+1];
+			for(int i = 1 ;i<=n;i++) {
+				for(int j = 1;j<=w;j++) {
+					if(j<arr[i][0]) {
+						dp[i][j]=dp[i-1][j];
+					}else {
+						dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j-arr[i][0]]+arr[i][1]);
+					}
+				}
+			}
 
-		for(int test_case = 1; test_case <= T; test_case++)
-		{
-            int n =sc.nextInt();
-            int k =sc.nextInt();
-            int[][] dp = new int[n+1][k+1];
-            int[][] arr= new int[n+1][2];
-            for(int i=1;i<=n;i++){
-            	int v =sc.nextInt();
-                int c = sc.nextInt();
-                arr[i][0]=v;
-                arr[i][1]=c;
-            }
-            for(int i =1;i<=n;i++){
-            	for(int j=1;j<=k;j++){
-                	if(arr[i][0]>j){
-                    	dp[i][j] = dp[i-1][j];
-                    }else{
-                    	dp[i][j]=Math.max(dp[i-1][j],dp[i-1][j-arr[i][0]]+arr[i][1]);
-                    }
-                }
-            }
-            System.out.println("#"+test_case+" "+dp[n][k]);
+			
+			System.out.println("#"+t+" "+dp[n][w]);
 		}
 	}
 }

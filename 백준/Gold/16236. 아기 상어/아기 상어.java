edeb6d2dbc346e452d5a0ccda34d808ec6af927b
이주호 <139448668+lsc713@users.noreply.github.com>
2024-09-29 02:00:08 +0900

    [Gold III] Title: 아기 상어, Time: 208 ms, Memory: 18628 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.java
+++ 백준/Gold/16236. 아기 상어/아기 상어.java
@@ -1,99 +1,69 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.*;
-class Main{
 
-    static int sharkX,sharkY,minX,minY,minDist;
-    static int[] dx = {-1,0,1,0};
-    static int[] dy = {0,-1,0,1};
-    static int[][] fish;
-    static int n,time;
-    static int size=2;
-    static int sharkStack=0;
+class Main{
+    static int n;
+    static int[] shark;
+    static List<int[]> fishList = new ArrayList<>();
     static int[][] map;
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        n = scanner.nextInt();
-        time =0;
-        map=new int[n][n];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                map[i][j] = scanner.nextInt();
-                if (map[i][j]==9){
-                    sharkX=i;
-                    sharkY=j;
+    static int sharkStack=0;
+    static int sharkSize=2;
+    static int[] dx = {-1,1,0,0};
+    static int[] dy = {0,0,-1,1};
+    static int answer=0;
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        n=sc.nextInt();
+        map= new int[n][n];
+        for(int i = 0;i<n;i++){
+            for(int j = 0 ; j <n;j++){
+                map[i][j]=sc.nextInt();
+                if(map[i][j]==9){
                     map[i][j]=0;
+                    shark = new int[]{i,j};
+                }else if(map[i][j]!=9&&map[i][j]!=0){
+                    fishList.add(new int[]{i,j});
                 }
             }
         }
         while(true){
-            fish=new int[n][n];
-            minX=Integer.MAX_VALUE;
-            minY=Integer.MAX_VALUE;
-            minDist=Integer.MAX_VALUE;
-
-            bfs(sharkX, sharkY);
-
-            if (minX != Integer.MAX_VALUE && minY != Integer.MAX_VALUE) {
-                sharkStack++;
-                map[minX][minY] = 0;
-                sharkX = minX;
-                sharkY = minY;
-                time += fish[minX][minY];
-
-                if (sharkStack == size) {
-                    size++;
-                    sharkStack = 0;
-                }
-            } else {
-                break;
+            int[] tmp = bfs();
+            if(tmp==null)break;
+            answer+=tmp[2];
+            map[tmp[0]][tmp[1]]=0;
+            sharkStack++;
+            shark=new int[]{tmp[0],tmp[1]};
+            if(sharkStack==sharkSize){
+                sharkStack=0;
+                sharkSize++;
             }
-
+            fishList.removeIf(fish->fish[0]==tmp[0]&&fish[1]==tmp[1]);
         }
-
-        System.out.println(time);
+        System.out.println(answer);
     }
-
-    private static void bfs(int sharkX, int sharkY) {
-        Queue<Pos> q = new LinkedList<>();
-        q.add(new Pos(sharkX, sharkY));
-        while (!q.isEmpty()) {
-            Pos pos = q.poll();
-            int x = pos.x;
-            int y = pos.y;
-            for (int i = 0; i < 4; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-                if(0<=nx&&nx<n&&0<=ny&&ny<n&&map[nx][ny]<=size&&fish[nx][ny]==0){
-                    fish[nx][ny]=fish[x][y]+1;
-                    if (map[nx][ny] != 0 && map[nx][ny] < size) {
-                        if(minDist>fish[nx][ny]){
-                            minDist=fish[nx][ny];
-                            minX=nx;
-                            minY=ny;
-                        }else if (minDist == fish[nx][ny]) {
-                            if (minX==nx){
-                                if (minY > ny) {
-                                    minX=nx;
-                                    minY=ny;
-                                }
-                            } else if (minX > nx) {
-                                minX = nx;
-                                minY = ny;
-                            }
-                        }
-                    }
-                    q.add(new Pos(nx, ny));
+    static int[] bfs(){
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->{
+            if(a[2]!=b[2])return a[2]-b[2];
+            if(a[0]!=b[0])return a[0]-b[0];
+            return a[1]-b[1];
+        });
+        pq.add(new int[]{shark[0],shark[1],0});
+        boolean[][] visited = new boolean[n][n];
+        visited[shark[0]][shark[1]]=true;
+        while(!pq.isEmpty()){
+            int[] tmp = pq.poll();
+            int x=tmp[0],y=tmp[1],d=tmp[2];
+            if(map[x][y]<sharkSize&&map[x][y]!=0){
+                return tmp;
+            }
+            for(int i = 0;i<4;i++){
+                int nx = x+dx[i];
+                int ny = y+dy[i];
+                if(0<=nx&&nx<n&&0<=ny&&ny<n&&!visited[nx][ny]&&map[nx][ny]<=sharkSize){
+                    pq.add(new int[]{nx,ny,d+1});
+                    visited[nx][ny]=true;
                 }
             }
         }
-    }
-    static class Pos{
-        int x,y;
-        public Pos(int x,int y){
-            this.x=x;
-            this.y=y;
-        }
+        return null;
     }
 }

b101ed5fa8cfa348809574278722bd8abc0826e1
이주호 <139448668+lsc713@users.noreply.github.com>
2024-11-07 13:42:45 +0900

    [D4] Title: 물놀이를 가자, Time: 1,537 ms, Memory: 119,680 KB -BaekjoonHub

--- SWEA/D4/10966. 물놀이를 가자/물놀이를 가자.java
+++ SWEA/D4/10966. 물놀이를 가자/물놀이를 가자.java
@@ -1,55 +1,57 @@
-import java.util.*;
-import java.io.FileInputStream;
 
 
-class Solution
-{
-    static int[] dx = {-1,1,0,0};
-    static int[] dy = {0,0,-1,1};
-    static int answer;
-    static boolean[][] visited;
-    static int n,m;
-    public static void main(String args[]) throws Exception
-    {
-        Scanner sc = new Scanner(System.in);
-        int T;
-        T=sc.nextInt();
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
 
-        for(int test_case = 1; test_case <= T; test_case++)
-        {
-            n = sc.nextInt();
-            m = sc.nextInt();
-            answer=0;
-            visited = new boolean[n][m];
-            Queue<int[]> q = new LinkedList<>();
-            for(int i = 0 ; i < n;i++){
-                String input = sc.next();
-                for(int j=0;j<m;j++){
-                    if(input.charAt(j)=='W'){
-                        visited[i][j] = true;
-                        q.offer(new int[]{i,j,0});
-                    }
-                }
-            }
-            while(!q.isEmpty()){
-                int[] now = q.poll();
-                int x = now[0];
-                int y = now[1];
-                int l = now[2];
-                answer += l;
-
-                for(int i = 0 ; i<4;i++){
-                    int nx = x+dx[i];
-                    int ny = y+dy[i];
-                    if(0<=nx&&nx<n&&0<=ny&&ny<m&&!visited[nx][ny]){
-                        visited[nx][ny]=true;
-                        q.offer(new int[]{nx,ny,l+1});
-                    }
-                }
-
-            }
-            System.out.println("#"+test_case+" "+answer);
-
-        }
-    }
+public class Solution {
+	static int[] dx = {-1,1,0,0};
+	static int[] dy = {0,0,-1,1};
+	static Queue<Node> q;
+	static class Node{
+		int x,y,result;
+		public Node(int x,int y, int result) {
+			this.x=x;
+			this.y=y;
+			this.result=result;
+		}
+	}
+	static int n,m,size;
+	static int sum;
+	static char[][] map;
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for(int t=1;t<=T;t++) {
+			n = sc.nextInt();
+			m = sc.nextInt();
+			map = new char[n][m];
+			boolean[][] visited = new boolean[n][m];
+			q = new LinkedList<>();
+			for(int i = 0 ; i < n;i++) {
+				String input = sc.next();
+				map[i]=input.toCharArray();
+				for(int j = 0 ; j <m;j++) {
+					if(map[i][j]=='W') {
+						visited[i][j]=true;
+						q.add(new Node(i,j,0));
+					}
+				}
+			}
+			sum=0;
+			while(!q.isEmpty()) {
+				Node cur = q.poll();
+				sum+=cur.result;
+				for(int a = 0 ; a <4;a++) {
+					int nx = cur.x+dx[a];
+					int ny = cur.y+dy[a];
+					if(0<=nx&&nx<n&&0<=ny&&ny<m&&!visited[nx][ny]) {
+						visited[nx][ny]=true;
+						q.add(new Node(nx,ny,cur.result+1));
+					}
+				}
+			}
+			System.out.println("#"+t+" " +sum);
+		}
+	}
 }

f1f4ce30e727443b83006d58648f820e45f243df
이주호 <139448668+lsc713@users.noreply.github.com>
2024-11-29 00:15:45 +0900

    Time: 2 ms (97.4%), Space: 41.8 MB (22.12%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -3,15 +3,15 @@ class Solution {
     public boolean isValid(String s) {
         Stack<Character> st = new Stack<>();
         for(char c : s.toCharArray()){
-            if (c == '(' || c == '[' || c == '{') {
+            if(c=='('||c=='['||c=='{'){
                 st.push(c);
             }else if(st.isEmpty()){
                 return false;
             }else{
                 char item = st.pop();
-                if ((c == ')' && item != '(') ||
-                    (c == ']' && item != '[') ||
-                    (c == '}' && item != '{')) {
+                if((c==')'&&item!='(')||
+                (c==']'&&item!='[')||
+                (c=='}'&&item!='{')){
                     return false;
                 }
             }

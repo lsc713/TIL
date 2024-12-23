ec495028085e050ea439452ba450e2cfe698fc4a
이주호 <139448668+lsc713@users.noreply.github.com>
2024-12-23 11:53:57 +0900

    Time: 0 ms (100%), Space: 45.3 MB (91.93%) - LeetHub

---
+++ 0792-binary-search/0792-binary-search.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Solution {
+    public int search(int[] nums, int target) {
+        int L=0;
+        int R = nums.length-1;
+        while(L<=R){
+            int mid = (L+R)/2;
+            if(nums[mid]==target)return mid;
+            else if(nums[mid]<target){
+                L=mid+1;
+            }else{
+                R=mid-1;
+            }
+        }
+        return -1;
+    }
+}

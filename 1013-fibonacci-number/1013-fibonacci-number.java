6b30cfa7bcb4bd236774c548600959b25140d9ca
이주호 <139448668+lsc713@users.noreply.github.com>
2024-12-27 13:04:54 +0900

    Time: 0 ms (100%), Space: 40.2 MB (78.23%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -2,11 +2,13 @@ class Solution {
     public int fib(int n) {
         return fibo(n);
     }
+    int[] memo = new int[31];
     int fibo(int n){
         if(n==0)return 0;
         if(n==1||n==2){
             return 1;
         }
-        return fibo(n-1)+fibo(n-2);
+        if(memo[n]>0)return memo[n];
+        return memo[n]=fibo(n-1)+fibo(n-2);
     }
 }

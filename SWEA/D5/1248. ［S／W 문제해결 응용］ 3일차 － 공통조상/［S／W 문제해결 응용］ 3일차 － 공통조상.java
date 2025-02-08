39c2b8a0c8720b1c3fe8a920bbce8070c74b0d70
이주호 <139448668+lsc713@users.noreply.github.com>
2025-02-08 11:36:15 +0900

    [D5] Title: [S/W 문제해결 응용] 3일차 - 공통조상, Time: 162 ms, Memory: 29,320 KB -BaekjoonHub

--- SWEA/D5/1248. ［S／W 문제해결 응용］ 3일차 － 공통조상/［S／W 문제해결 응용］ 3일차 － 공통조상.java
+++ SWEA/D5/1248. ［S／W 문제해결 응용］ 3일차 － 공통조상/［S／W 문제해결 응용］ 3일차 － 공통조상.java
@@ -1,46 +1,49 @@
+
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
 
 public class Solution {
+
     static int[] parent;
     static List<Integer>[] children;
     static boolean[] visited;
     static int V,E,v1,v2;
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
         int T = Integer.parseInt(br.readLine());
-        for(int t = 1;t<=T;t++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            V = Integer.parseInt(st.nextToken());
-            E = Integer.parseInt(st.nextToken());
-            parent = new int[V+1];
-            visited = new boolean[V+1];
-            children = new ArrayList[V+1];
-            for (int i = 1; i <= V; i++) {
-                children[i] = new ArrayList<>();
-            }
+        StringBuilder sb = new StringBuilder();
+        for (int t = 1; t <= T; t++) {
+
+            st = new StringTokenizer(br.readLine());
+             V = Integer.parseInt(st.nextToken());
+             E = Integer.parseInt(st.nextToken());
             v1 = Integer.parseInt(st.nextToken());
             v2 = Integer.parseInt(st.nextToken());
             st = new StringTokenizer(br.readLine());
+            parent = new int[V+1];
+            children = new ArrayList[V+1];
+            visited = new boolean[V+1];
+            for(int i = 1; i <= V; i++) {
+                children[i] = new ArrayList<>();
+            }
             for (int i = 0; i < E; i++) {
                 int p = Integer.parseInt(st.nextToken());
                 int c = Integer.parseInt(st.nextToken());
-                parent[c]=p;
+                parent[c]= p;
                 children[p].add(c);
             }
-            int cur=v1;
-            while (cur != 0) {
-                visited[cur] = true;
+            int cur = v1;
+            while(cur != 0){
+                visited[cur]=true;
                 cur = parent[cur];
             }
-            cur=v2;
+            cur = v2;
             int commonAncestor = v2;
-            while (cur != 0) {
+            while(cur != 0){
                 if(visited[cur]){
                     commonAncestor = cur;
                     break;
@@ -52,8 +55,8 @@ public class Solution {
         }
         System.out.println(sb);
     }
-    private static int subTreeSize(int ancestor){
-        int size =1;
+    private static int subTreeSize(int ancestor) {
+        int size = 1;
         for (int val : children[ancestor]) {
             size+=subTreeSize(val);
         }

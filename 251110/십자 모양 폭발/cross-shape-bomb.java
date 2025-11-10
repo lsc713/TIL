7edd68d60685a792d6947f4e132a0221ee8e5451
이주호 <139448668+lsc713@users.noreply.github.com>
2025-11-10 13:43:47 +0900

    Title: 십자 모양 폭발, Time: 749ms, Memory: 26MB, Status: Passed - Codetree

--- 251110/십자 모양 폭발/cross-shape-bomb.java
+++ 251110/십자 모양 폭발/cross-shape-bomb.java
@@ -6,13 +6,39 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
-        int[][] grid = new int[n][n];
+         grid = new int[n][n];
         for (int i = 0; i < n; i++)
             for (int j = 0; j < n; j++)
                 grid[i][j] = sc.nextInt();
         int r = sc.nextInt()-1;
         int c = sc.nextInt()-1;
         //폭탄크기정하기
+        bomb(r,c);
+
+        //각 열을 돌면서 아래에서 부터 당겨주기.
+        adjust();
+        
+        print();
+                
+        // Please write your code here.
+    }
+    static void adjust(){
+        int[][] temp = new int[n][n];
+        for(int j = 0 ; j < n ;j++){
+            int idx=n-1;
+            for(int i=n-1;i>=0;i--){
+                if(grid[i][j]!=0){
+                    temp[idx--][j]=grid[i][j];
+                }
+            }
+        }
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < n ; j++){
+                grid[i][j]=temp[i][j];
+            }
+        }
+    }
+    static void bomb(int r,int c){
         for(int d = 0 ; d < 4;d++){
             for(int j =1;j<=grid[r][c]-1;j++){
                 int nx = r+dx[d]*j;
@@ -23,28 +49,18 @@ public class Main {
             }
         }
         grid[r][c]=0;
-
-        //각 열을 돌면서 아래에서 부터 당겨주기.
-        for(int j = 0 ; j < n ;j++){
-            for(int i=n-1;i>=1;i--){
-                if(grid[i][j]==0){
-                    grid[i][j]=grid[i-1][j];
-                    grid[i-1][j]=0;
-                }
-            }
-        }
+    }
+    static int n;
+    static int[][] grid;
+    static boolean in(int nx,int ny){
+        return 0<=nx&&0<=ny&&ny<n&&nx<n;
+    }
+    static void print(){
         for (int i = 0; i < n; i++){
             for (int j = 0; j < n; j++){
                 System.out.print(grid[i][j]+" ");
             }
             System.out.println();
         }
-            
-                
-        // Please write your code here.
-    }
-    static int n;
-    static boolean in(int nx,int ny){
-        return 0<=nx&&0<=ny&&ny<n&&nx<n;
     }
 }

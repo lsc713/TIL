8df492c595f9809cc562763b4970b053bddacd3a
이주호 <139448668+lsc713@users.noreply.github.com>
2025-11-12 16:08:58 +0900

    Title: 떨어지는 1자 블록, Time: 168ms, Memory: 11MB, Status: Passed - Codetree

--- 251112/떨어지는 1자 블록/falling-horizontal-block.java
+++ 251112/떨어지는 1자 블록/falling-horizontal-block.java
@@ -1,57 +1,44 @@
 import java.util.Scanner;
 public class Main {
+    static int n,m,k;
+    static int[][] grid;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
          n = sc.nextInt();
          m = sc.nextInt();
          k = sc.nextInt()-1;
-        
-        grid = new int[n][n];
+         grid = new int[n][n];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 grid[i][j] = sc.nextInt();
             }
         }
 
-        //막대 집어넣기.
-        for(int i=k;i<k+m;i++){
-            grid[0][i]=1;
+        int trow = findrow();
+        for(int c=k;c<k+m;c++){
+            grid[trow][c]=1;
         }
-
-        //drop
-        drop();
-
-        
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                System.out.print(grid[i][j]+ " ");
+                System.out.print(grid[i][j]+" ");
             }
             System.out.println();
         }
         // Please write your code here.
     }
-    static int[][] grid;
-    static int n,m,k;
-    static void drop(){
-        boolean movable=true;
-        int row =0;
-        while(movable&&row<n){
-            if(n==1)break;
-            int i=row+1;
-            if(i>=n)break;
+    static int findrow(){
+        for(int row = 0 ; row < n-1 ; row++){
             for(int j=k;j<k+m;j++){
-                if(grid[i][j]==1){
-                    movable=false;
+                if(is1exist(row+1,j)){
+                    return row;
                 }
             }
-            if(movable){
-                for(int j=k;j<k+m;j++){
-                    grid[i-1][j]=0;
-                    grid[i][j]=1;
-                }
-            }       
-            row=i;
         }
+        return n-1;
+    }
+    static boolean is1exist(int x,int y){
+        if(grid[x][y]>0)return true;
+        return false;
     }
 }

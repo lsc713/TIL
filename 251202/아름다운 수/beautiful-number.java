ad5150a9acc57fe172cf8965a07082df0ff638f0
이주호 <139448668+lsc713@users.noreply.github.com>
2025-12-02 23:59:20 +0900

    Title: 아름다운 수, Time: 123ms, Memory: 10MB, Status: Passed - Codetree

--- 251202/아름다운 수/beautiful-number.java
+++ 251202/아름다운 수/beautiful-number.java
@@ -1,38 +1,23 @@
 import java.util.Scanner;
 public class Main {
+    static int cnt;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        cnt=0;
+        calc(0,n);
+        System.out.println(cnt);
         // Please write your code here.
-        System.out.println(count(n));
     }
-    static int count(int n){
-        int cnt=0;
-        int start = (int)Math.pow(10,n-1);
-        int end = (int)Math.pow(10,n)-1;
-        for(int i=start;i<=end;i++){
-            if(beautiful(i)){
-                cnt++;
+    static void calc(int idx,int current){
+        if(idx==current){
+            cnt++;
+            return;
+        }
+        for(int i =1;i<=4;i++){
+            if(idx+i<=current){
+                calc(idx+i,current);
             }
         }
-        return cnt;
-    }
-    static boolean beautiful(int num){
-        int[] arr = new int[5];
-        int temp = num;
-        while(temp>0){
-            int digit = temp%10;
-            if(digit<1||digit>4){
-                return false;
-            }
-            arr[digit]++;
-            temp/=10;
-        }
-        for(int i = 1;i<=4;i++){
-            if(arr[i]>0&&arr[i]!=i){
-                return false;
-            }
-        }
-        return true;
     }
 }

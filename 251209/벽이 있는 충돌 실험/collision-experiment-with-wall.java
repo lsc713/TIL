8f0a23debc14de86c25c0b4b6e4da71b607c52f5
이주호 <139448668+lsc713@users.noreply.github.com>
2025-12-09 16:45:53 +0900

    Title: 벽이 있는 충돌 실험, Time: 3091ms, Memory: 92MB, Status: Time Limit Exceed - Codetree

--- 251209/벽이 있는 충돌 실험/collision-experiment-with-wall.java
+++ 251209/벽이 있는 충돌 실험/collision-experiment-with-wall.java
@@ -11,7 +11,7 @@ public class Main {
     }
     static int[] dx = {-1,1,0,0};
     static int[] dy = {0,0,-1,1};
-    static char[] chars = {'U','D','R','L'};
+    static char[] chars = {'D','U','R','L'};
     static List<Node> list;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);

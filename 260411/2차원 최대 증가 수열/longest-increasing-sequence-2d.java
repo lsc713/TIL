80d60f9132a2436c61c9132dacc27554d84ff3bd
이주호 <139448668+lsc713@users.noreply.github.com>
2026-04-11 14:50:26 +0900

    Title: 2차원 최대 증가 수열, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260411/2차원 최대 증가 수열/longest-increasing-sequence-2d.java
+++ 260411/2차원 최대 증가 수열/longest-increasing-sequence-2d.java
@@ -11,12 +11,12 @@ public class Main {
         // Please write your code here.
         int ans =0;
         int[][] dp = new int[n][m];
+        dp[0][0]=1;
         for(int i = 0 ; i < n;i++){
             for(int j = 0 ; j < m ; j++){
-                dp[i][j]=1;
                 for(int k=0;k<i;k++){
                     for(int l=0;l<j;l++){
-                        if(grid[i][j]>grid[k][l] ){
+                        if(grid[i][j]>grid[k][l]&&dp[k][l]>0){
                             dp[i][j]=Math.max(dp[i][j],dp[k][l]+1);
                         }
                     }

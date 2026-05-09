b9b9ffd54518b8abb0d1bdcdd75b60af57e8f5fc
이주호 <139448668+lsc713@users.noreply.github.com>
2026-05-09 17:32:11 +0900

    [Passed] 막대기 나누기 (150ms, 10MB)

---
+++ 260509/막대기 나누기/rod-cutting.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] profit = new int[n+1];
+        for (int i = 1; i <= n; i++) {
+            profit[i] = sc.nextInt();
+        }
+        int[] dp = new int[n+1];
+        for(int i = 1 ; i <= n ; i++){
+            for(int j=i;j<=n;j++){
+                dp[j]=Math.max(dp[j],dp[j-i]+profit[i]);
+            }
+        }
+        System.out.println(dp[n]);
+
+        // Please write your code here.
+    }
+}
